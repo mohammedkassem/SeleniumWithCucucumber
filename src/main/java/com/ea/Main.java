39c4669a89c21d3444f44971f4a8e477a28780b5
mohammedkassem <35780964+mohammedkassem@users.noreply.github.com>
2020-04-10 19:52:03 +0300

    Update Main.java

--- src/main/java/com/ea/Main.java
+++ src/main/java/com/ea/Main.java
@@ -30,7 +30,7 @@ public class Main {
     public static void main(String[] args) {
 
         //Selenium 4
-        System.setProperty("webdriver.chrome.driver", "/Users/karthikkk/ChromeDriver/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "C:\Users\TechEye\Downloads\chromedriver_win32\chromedriver.exe");
         var chromeDriver = new ChromeDriver();
 
         var chromeDevTools = chromeDriver.getDevTools();
